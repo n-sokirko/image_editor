928fcbfff3f92ef4831d4ca114da3f635009346d
Nikita Sokirko <sokirko1206@gmail.com>
2023-06-09 17:17:11 +0200

    added_functionality_for_upload_button

--- image_editor_window.py
+++ image_editor_window.py
@@ -11,6 +11,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from PyQt5.QtWidgets import QApplication, QStyleFactory
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QStyleFactory, QFileDialog, QHBoxLayout
+
 
 
 class Ui_MainWindow(object):
@@ -20,6 +23,16 @@ class Ui_MainWindow(object):
         MainWindow.setStyleSheet("background-color: rgb(90, 39, 86);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+
+        self.image_layout = QHBoxLayout()
+        self.image_label = QtWidgets.QLabel(self.centralwidget)
+        self.image_label.setObjectName("image_label")
+        self.image_label.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)  
+        self.image_label.setAlignment(QtCore.Qt.AlignCenter)  
+        self.image_label.setMaximumSize(800, 800)
+        self.image_layout.addWidget(self.image_label)        
+
         self.Brightness = QtWidgets.QSlider(self.centralwidget)
         self.Brightness.setGeometry(QtCore.QRect(30, 240, 191, 22))
         self.Brightness.setAutoFillBackground(False)
@@ -28,6 +41,7 @@ class Ui_MainWindow(object):
         self.Brightness.setOrientation(QtCore.Qt.Horizontal)
         self.Brightness.setTickPosition(QtWidgets.QSlider.TicksBelow)
         self.Brightness.setObjectName("Brightness")
+
         self.Blacknes = QtWidgets.QSlider(self.centralwidget)
         self.Blacknes.setGeometry(QtCore.QRect(30, 280, 191, 22))
         self.Blacknes.setSliderPosition(50)
@@ -35,51 +49,78 @@ class Ui_MainWindow(object):
         self.Blacknes.setTickPosition(QtWidgets.QSlider.TicksBelow)
         self.Blacknes.setTickInterval(0)
         self.Blacknes.setObjectName("Blacknes")
+
         self.Contract = QtWidgets.QSlider(self.centralwidget)
         self.Contract.setGeometry(QtCore.QRect(30, 320, 191, 22))
         self.Contract.setSliderPosition(50)
         self.Contract.setOrientation(QtCore.Qt.Horizontal)
         self.Contract.setTickPosition(QtWidgets.QSlider.TicksBelow)
         self.Contract.setObjectName("Contract")
+
         self.Blur = QtWidgets.QSlider(self.centralwidget)
         self.Blur.setGeometry(QtCore.QRect(30, 360, 191, 22))
         self.Blur.setSliderPosition(50)
         self.Blur.setOrientation(QtCore.Qt.Horizontal)
         self.Blur.setTickPosition(QtWidgets.QSlider.TicksBelow)
         self.Blur.setObjectName("Blur")
+
         self.Undo_butt = QtWidgets.QPushButton(self.centralwidget)
         self.Undo_butt.setGeometry(QtCore.QRect(20, 190, 93, 28))
         self.Undo_butt.setStyleSheet("color: rgb(255, 255, 255);\n"
 "font: 8pt \"Yu Gothic\";")
         self.Undo_butt.setObjectName("Undo_butt")
+
         self.Remove_butt = QtWidgets.QPushButton(self.centralwidget)
         self.Remove_butt.setGeometry(QtCore.QRect(130, 190, 93, 28))
         self.Remove_butt.setStyleSheet("color: rgb(255, 255, 255);\n"
 "font: 8pt \"Yu Gothic\";")
         self.Remove_butt.setObjectName("Remove_butt")
+
         self.Save_butt = QtWidgets.QPushButton(self.centralwidget)
         self.Save_butt.setGeometry(QtCore.QRect(20, 150, 201, 28))
         self.Save_butt.setStyleSheet("color: rgb(255, 255, 255);\n"
 "font: 8pt \"Yu Gothic\";")
         self.Save_butt.setObjectName("Save_butt")
+
         self.Saveas_butt = QtWidgets.QPushButton(self.centralwidget)
         self.Saveas_butt.setGeometry(QtCore.QRect(20, 110, 201, 28))
         self.Saveas_butt.setStyleSheet("color: rgb(255, 255, 255);\n"
 "font: 8pt \"Yu Gothic\";")
         self.Saveas_butt.setObjectName("Saveas_butt")
+
         self.Upload_butt = QtWidgets.QPushButton(self.centralwidget)
         self.Upload_butt.setGeometry(QtCore.QRect(20, 70, 201, 28))
         self.Upload_butt.setStyleSheet("color: rgb(255, 255, 255);\n"
-"font: 8pt \"Yu Gothic\";")
+                                        "font: 8pt \"Yu Gothic\";")
         self.Upload_butt.setObjectName("Upload_butt")
+        self.Upload_butt.clicked.connect(self.upload_image_clicked)
+
+        
+        self.main_layout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.main_layout.addLayout(self.image_layout)
+
         MainWindow.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        
+        
         slider_color = QtGui.QColor(0, 255, 204)
         slider_style = "QSlider::handle:horizontal { background-color: %s; }" % slider_color.name()
         QApplication.setStyle(QStyleFactory.create('Fusion'))
         app.setStyleSheet(slider_style)
+        self.image_label.setPixmap(QtGui.QPixmap())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        
+    def upload_image_clicked(self):
+        file_dialog = QFileDialog()
+        file_dialog.setNameFilter("Images (*.png *.xpm *.jpg *.bmp)")
+        file_dialog.setFileMode(QFileDialog.ExistingFile)
+        if file_dialog.exec_():
+            selected_file = file_dialog.selectedFiles()[0]
+            pixmap = QtGui.QPixmap(selected_file)
+            self.image_label.setPixmap(pixmap.scaled(self.image_label.size(), QtCore.Qt.KeepAspectRatio)) 
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -89,6 +130,7 @@ class Ui_MainWindow(object):
         self.Save_butt.setText(_translate("MainWindow", "Save Image"))
         self.Saveas_butt.setText(_translate("MainWindow", "Save As"))
         self.Upload_butt.setText(_translate("MainWindow", "Upload Image"))
+   
 
 
 if __name__ == "__main__":
@@ -99,3 +141,4 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+    
